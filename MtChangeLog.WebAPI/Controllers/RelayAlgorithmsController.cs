290f1382f077020e138d4a91dffbe3e8d24ae6e9
Andrey G <georgievskiy.x64@gmail.com>
2021-09-26 23:16:30 +0300

    Добавлен: RelayAlgorithmsController

---
+++ MtChangeLog.WebAPI/Controllers/RelayAlgorithmsController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MtChangeLog.DataBase.Repositories.Interfaces;
+using MtChangeLog.DataObjects.Entities.Base;
+using MtChangeLog.DataObjects.Entities.Editable;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MtChangeLog.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelayAlgorithmsController : ControllerBase
+    {
+        private readonly IRelayAlgorithmsRepository repository;
+
+        public RelayAlgorithmsController(IRelayAlgorithmsRepository repository) 
+        {
+            this.repository = repository;
+        }
+        
+        // GET: api/<RelayAlgorithmsController>
+        [HttpGet]
+        public IEnumerable<RelayAlgorithmBase> Get()
+        {
+            var result = this.repository.GetEntities();
+            return result;
+        }
+
+        // GET api/<RelayAlgorithmsController>/00000000-0000-0000-0000-000000000000
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                var result = this.repository.GetEntity(id);
+                return this.Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return this.NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<RelayAlgorithmsController>/default
+        [HttpGet("default")]
+        public IActionResult GetDefault()
+        {
+            return this.Ok(RelayAlgorithmBase.Default);
+        }
+
+        // POST api/<RelayAlgorithmsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] RelayAlgorithmBase entity)
+        {
+            try
+            {
+                this.repository.AddEntity(entity);
+                return this.Ok($"Relay algorithm {entity.ANSI} {entity.Title} adding to the database");
+            }
+            catch (ArgumentException ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<RelayAlgorithmsController>/00000000-0000-0000-0000-000000000000
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] RelayAlgorithmBase entity)
+        {
+            try
+            {
+                if (id != entity.Id)
+                {
+                    throw new ArgumentException($"url id = {id} is not equal to entity id = {entity.Id}");
+                }
+                this.repository.UpdateEntity(entity);
+                return this.Ok($"Relay algorithm {entity.ANSI} {entity.Title} update in the database");
+            }
+            catch (ArgumentException ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<RelayAlgorithmsController>/00000000-0000-0000-0000-000000000000
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                this.repository.DeleteEntity(id);
+                return this.Ok();
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+        }
+    }
+}
