be72fab033cd522481bc4db1283bc8270a7be15a
Andrey G <georgievskiy.x64@gmail.com>
2021-11-23 22:34:24 +0300

    Добавлены пакеты swagger в проект

--- MtChangeLog.WebAPI/Startup.cs
+++ MtChangeLog.WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 using MtChangeLog.DataBase.Contexts;
 using MtChangeLog.DataBase.Repositories.Interfaces;
@@ -62,6 +63,11 @@ namespace MtChangeLog.WebAPI
             {
                 configure.JsonSerializerOptions.Converters.Add(new DateTimeConverter());
             });
+
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MtChangeLog.WebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +78,8 @@ namespace MtChangeLog.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MtChangeLog.WebAPI v1"));
             }
 
             app.UseDefaultFiles();
