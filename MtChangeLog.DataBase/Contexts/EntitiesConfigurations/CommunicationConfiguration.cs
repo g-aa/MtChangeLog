13f6167ea72e838fbda165e08508b1df217f2def
Andrey G <georgievskiy.x64@gmail.com>
2021-12-11 22:29:37 +0300

    Добавлена настройка конфигурации таблицы Communication

---
+++ MtChangeLog.DataBase/Contexts/EntitiesConfigurations/CommunicationConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MtChangeLog.DataBase.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtChangeLog.DataBase.Contexts.EntitiesConfigurations
+{
+    internal class CommunicationConfiguration : IEntityTypeConfiguration<DbCommunication>
+    {
+        public void Configure(EntityTypeBuilder<DbCommunication> builder)
+        {
+            builder.ToTable("Communication");
+            builder.HasComment("Таблица с перечнем коммуникационных модулей поддерживаемых в блоках БМРЗ-100/120/150/160");
+
+            builder.Property(e => e.Protocols)
+                .HasDefaultValue("ModBus-MT")
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(e => e.Description)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(e => e.Default)
+                .HasDefaultValue(false)
+                .IsRequired();
+        }
+    }
+}
