a61104682f13a207713fad00af6414183c119d48
Andrey Georgievskiy <SESA494515@se.com>
2022-01-13 10:22:26 +0300

    Добавление наследования в Editable DTO  от ShortView

--- MtChangeLog.DataObjects/Entities/Editable/AuthorEditable.cs
+++ MtChangeLog.DataObjects/Entities/Editable/AuthorEditable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MtChangeLog.DataObjects.Entities.Views.Shorts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,25 +8,15 @@ using System.Threading.Tasks;
 
 namespace MtChangeLog.DataObjects.Entities.Editable
 {
-    public class AuthorEditable
+    public class AuthorEditable : AuthorShortView
     {
-        public Guid Id { get; set; }
-        
-        [Required(ErrorMessage = "Имя автора параметр обязательный для заполнения")]
-        [StringLength(32, ErrorMessage = "Имя должно содержать не больше 32 символов")]
-        public string FirstName { get; set; }
-        
-        [Required(ErrorMessage = "Фамилия автора параметр обязательный для заполнения")]
-        [StringLength(32, ErrorMessage = "Фамилия должна содержать не больше 32 символов")]
-        public string LastName { get; set; }
-        
         [Required(AllowEmptyStrings = true)]
         [StringLength(250, ErrorMessage = "Должность автора должна содержать не больше 250 символов")]
         public string Position { get; set; }
 
         public override string ToString()
         {
-            return $"{this.LastName} {this.FirstName}";
+            return $"{base.ToString()}, {this.Position}";
         }
     }
 }
