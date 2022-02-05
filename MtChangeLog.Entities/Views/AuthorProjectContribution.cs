d64ffef0e05e304dd9c30fd9c78e272eb4be15ae
Andrey G <georgievskiy.x64@gmail.com>
2022-02-05 22:27:49 +0300

    Переработка структуры проекта: создание модуля Entities

---
+++ MtChangeLog.Entities/Views/AuthorProjectContribution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtChangeLog.Entities.Views
+{
+    public class AuthorProjectContribution
+    {
+        public string Name { get; set; }
+        public string Project { get; set; }
+        public int ContributionCount { get; set; }
+    }
+}
