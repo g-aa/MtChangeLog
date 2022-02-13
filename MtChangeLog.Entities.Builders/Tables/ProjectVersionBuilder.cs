4dd60d8af8ac0d6f61385676140205a24c22a9ae
Andrey G <georgievskiy.x64@gmail.com>
2022-02-13 15:39:04 +0300

    Добавление модуля MtChangeLog.Entities.Builders,

---
+++ MtChangeLog.Entities.Builders/Tables/ProjectVersionBuilder.cs
@@ -0,0 +1,58 @@
+﻿using MtChangeLog.Entities.Tables;
+using MtChangeLog.TransferObjects.Editable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtChangeLog.Entities.Builders.Tables
+{
+    public class ProjectVersionBuilder
+    {
+        private readonly ProjectVersion project;
+
+        public ProjectVersionBuilder(ProjectVersion project) 
+        {
+            this.project = project;
+        }
+
+        public ProjectVersionBuilder SetAttributes(ProjectVersionEditable editable)
+        { 
+            return this;
+        }
+
+        public ProjectVersionBuilder SetPlatform(Platform platform) 
+        {
+            return this;
+        }
+
+        public ProjectVersionBuilder SetAnalogModule(AnalogModule module) 
+        {
+            return this;
+        }
+
+        public ProjectVersionBuilder SetProjectStatus(ProjectStatus status) 
+        {
+            return this;
+        }
+
+        public ProjectVersion Build()
+        {
+            return this.project;
+        }
+
+        public static ProjectVersionBuilder GetBuilder() 
+        {
+            return new ProjectVersionBuilder(new ProjectVersion());
+        }
+    }
+
+    public static class ProjectVersionExtension
+    {
+        public static ProjectVersionBuilder GetBuilder(this ProjectVersion project) 
+        {
+            return new ProjectVersionBuilder(project);
+        }
+    }
+}
