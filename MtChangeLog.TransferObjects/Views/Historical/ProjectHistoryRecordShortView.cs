bc53a7af3d88b85ab3f3ec1cec3a8d6e54514c60
Andrey G <georgievskiy.x64@gmail.com>
2022-02-20 17:08:53 +0300

    Добавление ProjectHistoryRecord ShortView и View в MtChangeLog.TransferObjects

---
+++ MtChangeLog.TransferObjects/Views/Historical/ProjectHistoryRecordShortView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtChangeLog.TransferObjects.Views.Historical
+{
+    public class ProjectHistoryRecordShortView
+    {
+        public Guid ProjectRevisionId { get; set; }
+        /// <summary>
+        /// наименование проекта, комбинация: "ProjectVersion.Prefix"-"ProjectVersion.Title"-"ProjectVersion.Version"_"ProjectRevision.Revision"
+        /// </summary>
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Platform { get; set; }
+    }
+}
